4cec16e7b3327443d7e53284dc9727a92f15986c
srhLzbth <bluewhirligig@gmail.com>
2016-10-26 11:04:44 -0400

    Added displayName property to location.

---
+++ app/DoctrineMigrations/Version20161026162028.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20161026162028 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE location CHANGE description display_name VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE location ADD description VARCHAR(255) DEFAULT NULL AFTER display_name');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+    }
+}
