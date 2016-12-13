9fd967a09dac92a7e847926248f4f60fe1dca602
srhLzbth <bluewhirligig@gmail.com>
2016-12-13 11:26:50 -0800

    Added buildSelectElem and buildSimpleOpts. Refactored buildElem.

--- web/bundles/app/js/util.js
+++ web/bundles/app/js/util.js
@@ -1,8 +1,10 @@
 (function(){ 
     var eif = ECO_INT_FMWK;
     eif.util = {
+        buildElem: buildElem,
+        buildSelectElem: buildSelectElem,
+        buildSimpleOpts: buildSimpleOpts,
         ucfirst: ucfirst, 
-        createElem: createElem,
     };
 
     extendJquery();
@@ -13,25 +15,63 @@
     }
 
     /*------------ HTML ------------------------------------------------------*/
-    function createElem(tag, attrs) {                                           //console.log("createElem called. tag = %s. attrs = %O", tag, attrs);// attr = { id, class, name, type, value, text }
+    /*------------ HTML Functions -------------------------------------------*/
+    function buildElem(tag, attrs) {                                           //console.log("buildElem called. tag = %s. attrs = %O", tag, attrs);// attr = { id, class, name, type, value, text }
         var elem = document.createElement(tag);
-        if (attrs) {
-            elem.id = attrs.id || '';
-            elem.className = attrs.class || '';   //Space separated classNames
-
-            if (attrs.text) { $(elem).text(attrs.text); }
-
-            if (attrs.name || attrs.type || attrs.value ) { 
-                $(elem).attr({
-                    name: attrs.name   || '', 
-                    type: attrs.type   || '',
-                    value: attrs.value || '',
-                    placeholder: attrs.placeholder || '',
-                }); 
-            }
-        }
+        if (attrs) { addAttributes(elem, attrs); }
         return elem;
     }
+    function addAttributes(elem, attrs) {
+        elem.id = attrs.id || '';
+        elem.className = attrs.class || '';   //Space separated classNames
+
+        if (attrs.text) { $(elem).text(attrs.text); }
+
+        if (attrs.name || attrs.type || attrs.value ) { 
+            $(elem).attr({
+                name: attrs.name   || '', 
+                type: attrs.type   || '',
+                value: attrs.value || '',
+                placeholder: attrs.placeholder || '',
+            }); 
+        }
+    }
+    /**
+     * Builds a select drop down with the options, attributes and change method 
+     * passed. Sets the selected option as the passed 'selected' or the default 'all'.
+     */
+    function buildSelectElem(options, attrs, changeFunc, selected) {
+        var selectElem = buildElem('select', attrs); 
+        var selected = selected || 'all';
+        
+        options.forEach(function(opts){
+            $(selectElem).append($("<option/>", {
+                value: opts.value,
+                text: opts.text
+            }));
+        });
+
+        if (attrs) { addAttributes(selectElem, attrs); }
+        $(selectElem).val(selected);
+        $(selectElem).change(changeFunc);
+        return selectElem;
+    }
+    /**
+     * Creates an opts obj for each 'item' in array with the index as the value and 
+     * the 'item' as the text.
+     */
+    function buildSimpleOpts(optAry, placeholder) {   console.log("buildSimpleOpts(optAry= %O, placeholder= %s)", optAry, placeholder);
+        var opts = []
+        optAry.forEach(function(option, idx){
+            opts.push({
+                value: idx.toString(),
+                text: option  });
+        });
+        if (placeholder) {
+            opts.unshift({ value: "placeholder", text: placeholder });
+        }
+        return opts;
+    }   
     /*--------------------------Jquery Extensions-----------------------------*/
         function extendJquery() {
             addOnEnterEvent();
