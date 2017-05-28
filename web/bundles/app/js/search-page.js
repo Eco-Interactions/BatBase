341e0a0bf77d623c1cc88e018a4a58b8695637fd
SrhLzbthYngr <rnbwspctrm@gmail.com>
2017-05-28 10:44:34 -0700

    Added id to tree node tooltips.

--- web/bundles/app/js/search-page.js
+++ web/bundles/app/js/search-page.js
@@ -1635,7 +1635,7 @@
     }
     function getPencilHtml(id, entity, editFunc) {
         var editPencil = `<img src="../bundles/app/images/eif.pencil.svg" id="edit`+id+`"
-            class="grid-edit" title="Edit `+entity+`" alt="Edit `+entity+`">`;
+            class="grid-edit" title="Edit `+entity+` `+id+`" alt="Edit `+entity+`">`;
         $('#search-grid').off('click', '#edit'+id);
         $('#search-grid').on(
             'click', '#edit'+id, eif.crud.editEntity.bind(null, id, _util.lcfirst(entity)));
