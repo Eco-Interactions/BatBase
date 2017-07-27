4a53f886d56a5bd3a3e19cd4f24066eb20a252c3
SrhLzbthYngr <rnbwspctrm@gmail.com>
2017-07-27 09:03:52 -0700

    (TEST)

--- web/bundles/app/js/search-page.js
+++ web/bundles/app/js/search-page.js
@@ -15,7 +15,7 @@
      *      data should be cleared and redownloaded.
      */
     var userRole, miscObj = {}, columnDefs = [], gParams = {}; 
-    var dataKey = 'Welcome to the past, again.';
+    var dataKey = 'Welcome to tomorrow.';
     var eif = ECO_INT_FMWK;
     var _util = eif.util;
     var localStorage = _util.setlocalStorage();
