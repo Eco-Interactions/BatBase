12d0059de6e3236cce7375c2b640ee55af595ff5
SrhLzbthYngr <rnbwspctrm@gmail.com>
2017-08-01 10:22:57 -0700

    DB MIGRATION. Fixed a bug in the source data clean-up code.
    UPdated todo.

--- app/DoctrineMigrations/Version20170725215243RmvSrcs.php
+++ app/DoctrineMigrations/Version20170725215243RmvSrcs.php
@@ -29,25 +29,63 @@ class Version20170725215243RmvSrcs extends AbstractMigration implements Containe
         $admin = $em->getRepository('AppBundle:User')->findOneBy(['id' => 6]);
 
         $this->removeSrcs($admin, $em);
-        $em->flush();
     }
     private function removeSrcs($admin, &$em)
     {
-        $srcs = [ 801, 452, 918, 953, 807, 796, 954, 767, 854, 720, 465 ];
-        foreach ($srcs as $srcId) {
+        $srcs = [ 801, 770, 452, 918, 953, 807, 796, 954, 767, 854, 720, 465 ];
+        foreach ($srcs as $srcId) {                                             print('processing src = '. $srcId."\n");
             $src = $this->getEntity($srcId, 'Source', $em);
-            $srcType = $src->getSourceType()->getDisplayName();
-            $getSrcType = 'get' . $srcType;
-            $detail = $this->getEntity($src->$getSrcType()->getId(), $srcType, $em);  print('detail = '.$detail->getId());
+            $detail = $this->getDetailEntity($src, $em);
+
             $src->setUpdatedBy($admin);
             $detail->setUpdatedBy($admin);
+            
             $em->remove($detail);
+            
+            $this->removeChildSrcs($src, $em);
+            $this->removeFromContributors($src, $em);
+            $this->removeFromParentSrc($src, $em);
+            
             $em->remove($src);            
+            $em->flush();   
         }
+    }
+    private function getDetailEntity($src, $em)
+    {
+        $srcType = $src->getSourceType()->getDisplayName();
+        $getSrcType = 'get' . $srcType;
+        return $this->getEntity($src->$getSrcType()->getId(), $srcType, $em);   //print('detail = '.$detail->getId());
     }
     private function getEntity($id, $entity, $em)
     {
         return $em->getRepository('AppBundle:'.$entity)->findOneBy(['id' => $id]);
+    }
+    private function removeFromParentSrc($src, &$em)
+    {                                                                           //print("removing parent\n");
+        $prnt = $src->getParentSource();
+        if (!$prnt) { return; }  
+        $src->setParentSource(null);    
+        $prnt->removeChildSource($src);                                         
+        $em->persist($src);   
+        $em->persist($prnt);
+    }
+    private function removeChildSrcs($src, &$em)
+    {
+        foreach ($src->getChildSources() as $child) {
+            $src->removeChildSource($child);
+            $this->removeFromContributors($child, $em);
+            $em->persist($child);
+            $em->remove($child);   
+        }
+    }
+    private function removeFromContributors($src, &$em)
+    {
+        foreach ($src->getContributors() as $contrib) {
+            $auth = $contrib->getAuthorSource(); 
+            $auth->removeContribution($contrib);
+            $src->removeContributor($contrib);
+            $em->persist($auth);
+        }
     }
     /**
      * @param Schema $schema
