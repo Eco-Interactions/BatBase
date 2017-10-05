71a8f17daa665122f77ffb8b6c10d0b3b3dc9e36
SrhLzbthYngr <rnbwspctrm@gmail.com>
2017-10-05 10:19:21 -0700

    Renamed feature to be specific to init state. Refactored context.

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -3,6 +3,7 @@
 use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
+// use Behat\MinkExtension\Context\MinkContext;
 use Behat\MinkExtension\Context\RawMinkContext;
 
 require_once(__DIR__.'/../../vendor/symfony/phpunit-bridge/bin/.phpunit/phpunit-5.7/vendor/autoload.php');
@@ -37,25 +38,84 @@ class FeatureContext extends RawMinkContext implements Context
         fwrite(STDOUT, "\033[u");
         return;
     }
-
+    /** --------------------------- Database Funcs ---------------------------*/
     /**
      * @Given the database has loaded
      */
     public function theDatabaseHasLoaded()
     {
         $this->getSession()->wait( 5000, "$('.ag-row').length" );
-        $row = $this->getSession()->getPage()->find('css', '.ag-row');
-        assertNotNull($row, "Can not find .ag-row");
+        $row = $this->getSession()->getPage()->find('css', '[row=0]');
+        assertNotNull($row, "There are no rows in the database grid.");
     }
+
+    /** -------------------------- Page Interactions --------------------------*/
 
     /**
      * @Given I exit the tutorial
      */
     public function iExitTheTutorial()
     {
+        $tutorial = $this->getSession()->getPage()->find('css', '.intro-tips');
+        assertNotNull($tutorial, 'Tutorial is not displayed.');
         $this->getSession()->executeScript("$('.introjs-overlay').click();");
         sleep(1);
     }
+
+    /**
+     * @Given the database grid is in :view view
+     */
+    public function theDatabaseGridIsInSelectedView($view)
+    {
+        $vals = ['Taxon' => 'taxa', 'Location' => 'locs', 'Source' => 'srcs'];
+        $this->getSession()->executeScript("$('#search-focus').val('$vals[$view]').change();");
+        $selected = $this->getSession()->evaluateScript("$('#search-focus option:selected').text();");
+        assertEquals($selected, $view, 'In "'.$selected.'" view; expected "'.$view.'"');
+        sleep(1);
+    }
+
+    /**------------------ Grid Funcs -----------------------------------------*/
+    /**
+     * @Then the count column should show :count interactions
+     */
+    public function theCountColumnShouldShowInteractions($count)
+    {
+        $cell = $this->getSession()->getPage()->find('css', '[row=0] [colId="intCnt"]');
+        assertContains($cell->getText(), $count, 'No interaction count found.');
+    }
+    /**
+     * @Then data in the interaction rows
+     */
+    public function dataInTheInteractionRows()
+    {   /** Data pulled from the Subject Taxon column. */
+        $data = $this->getSession()->getPage()->find('css', '[colid="subject"] span');
+        assertNotNull($data->getText(), 'No data found in the interaction rows.');
+    }
+
+    /**
+     * @Then I should see :count rows in the grid data tree
+     */
+    public function iShouldSeeRowsInTheGridDataTree($count)
+    {
+        $rows = $this->getSession()->getPage()->findAll('css', '.ag-body-container>div'); 
+        assertCount(intval($count), $rows, 'There are "'.count($rows).'" rows displayed; Expected "'.$count.'"');
+    }
+
+    /**
+     * @Then I should see :text in the tree
+     */
+    public function iShouldSeeInTheTree($text)
+    {   
+        $treeNodes = $this->getSession()->getPage()->findAll('css', '[colid="name"] span.ag-group-value span'); 
+        assertNotNull($treeNodes);  
+        $found = false;
+        for ($i=0; $i < count($treeNodes); $i++) { 
+            if ($treeNodes[$i]->getText() === $text) { $found = true; break;}
+        }
+        assertTrue($found, '"'.$text.'" is not displayed in grid data-tree.');
+    }
+
+    /** ------------------Taxon View -----------------------------------------*/
 
     /**
      * @When I group taxa by :realm
@@ -65,8 +125,7 @@ class FeatureContext extends RawMinkContext implements Context
         $vals = ['Bat' => 2, 'Plant' => 3, 'Arthropod' => 4];
         $this->getSession()->executeScript("$('#sel-domain').val($vals[$realm]).change();");
         $selected = $this->getSession()->evaluateScript("$('#sel-domain option:selected').text();");
-
-        assertEquals($selected, $realm, 'Taxa grouped by $selected; Expected $realm');
+        assertEquals($selected, $realm, 'Taxa grouped by "'.$selected.'"; Expected "'.$realm.'"');
     }
 
 
