cc532ed74d00c8c0c45b71ba7cd272a7d1514003
srhLzbth <rnbwspctrm@gmail.com>
2019-02-10 12:37:39 -0800

    Refactored custom markers

--- assets/js/db/map-markers.js
+++ assets/js/db/map-markers.js
@@ -257,25 +257,31 @@ function bindClassContextToMethods(self) {
     }
 }
 /** ------- Shared Helpers --------- */
-function getCustomIcon(iconType) { 
+function getCustomIcon(iconType) {                                              //console.log('iconType = ', iconType)
     if (!iconType || iconType && iconType.includes('form')) { return null; }    //console.log('returning custom icon');
-    return {icon:  L.icon({
-        iconUrl: require('./../../css/images/teal-marker-icon.png'),
-        iconSize: [29, 43],
-        iconAnchor: [16, 42],
-        popupAnchor: [0, -39],
-        shadowUrl: require('./../../css/images/marker-shadow.png'),
-        shadowSize: [33, 45],
-        shadowAnchor: [10, 44]
-    })};
-
-    // const classes = iconType || 'single-marker info';
-    // return {
-    //     icon: L.divIcon({
-    //         className: classes,
-    //         html: iconType === 'new-loc' ? '' : 1,
-    //     })
-    // }
+    return iconType === 'edit-loc' ? getTealPinMarker() : getGreenCircleMarker();
+}
+function getTealPinMarker() {
+    return { 
+        icon:  L.icon({
+            iconUrl: require('./../../css/images/teal-marker-icon.png'),
+            iconSize: [29, 43],
+            iconAnchor: [16, 42],
+            popupAnchor: [0, -39],
+            shadowUrl: require('./../../css/images/marker-shadow.png'),
+            shadowSize: [33, 45],
+            shadowAnchor: [10, 44]
+        })
+    };
+}
+function getGreenCircleMarker() {  
+    const classes = iconType || 'single-marker info';
+    return {
+        icon: L.divIcon({
+            className: 'single-marker info',
+            html: iconType === 'new-loc' ? '' : 1,
+        })
+    }
 }
 /** ---------------- Interaction Marker/Popup Helpers ----------------------  */
 function getIntPopupHtml(focus, intData) {                                      //console.log('getIntPopupHtml. intData = %O', intData);
