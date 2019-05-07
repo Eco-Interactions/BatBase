3b01694b6fda22cdf8e541f7032c95cf97103810
srhLzbth <rnbwspctrm@gmail.com>
2019-05-07 13:06:39 -0700

    Modified pop-out-panel styles. Data list panel init state complete

--- assets/js/db/db-table/save-ints.js
+++ assets/js/db/db-table/save-ints.js
@@ -15,11 +15,26 @@ export function toggleSaveIntsPanel() {                                         
     } else { hideIntPanel(); }
 }
 function buildAndShowIntPanel() {                                               console.log('buildAndShowIntPanel')
+    showPanel();
+    disableInputs();
+    initListCombobox();
+}
+function showPanel() {
     $('#int-opts').removeClass('closed');  
     $('#db-opts-col4').addClass('shw-col-borders hide-int-bttm-border');
+    window.setTimeout(function() { 
+        $('#int-opts').css('overflow-y', 'visible');
+        $('#saved-ints')[0].selectize.focus();  
+    }, 500);  
+}
+function initListCombobox() {
     _u.initCombobox('Int-lists');   
     $('#saved-ints')[0].selectize.clear();  //Can't figure out why an option is being selected
-    window.setTimeout(function() { $('#int-opts').css('overflow-y', 'visible')}, 500);  
+}
+function disableInputs() {
+    $('#int-opts input, #list-details textarea, #mod-list-pnl label, #int-opts button')
+        .attr({'disabled': 'disabled'}).css({'opacity': '.5'});
+    // $('label[for="add-all-list"], label[for="add-one-list"]').css({'opacity': '.5'});
 }
 function hideIntPanel() {                                                       console.log('hideIntPanel')
     $('#int-opts').css('overflow-y', 'hidden');
