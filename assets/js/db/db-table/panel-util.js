d7e01661fbf7e92b514aea3958acdf8e23ce2f00
srhLzbth <rnbwspctrm@gmail.com>
2019-06-16 12:34:53 -0700

    Foundation laid for vertical panels

--- assets/js/db/db-table/panel-util.js
+++ assets/js/db/db-table/panel-util.js
@@ -33,19 +33,8 @@ export function closeOpenPanels() {
 export function togglePanel(id, state) {
     const col = getMenuColumn(id);
     const colClass = getColumnSpacerId(id);
-    if (state === 'open') { cssOpenPanel(id, col, colClass) }
-    else { cssClosePanel(id, col, colClass) }
-}
-function cssOpenPanel(id, col, colClass) {
-    $(id).removeClass('closed');  
-    $(col).addClass('shw-col-borders ' + colClass);
-    window.setTimeout(function() { 
-        $(id).css('overflow-y', 'visible')}, 500);  
-}
-function cssClosePanel(id, col, colClass) {
-    $(id).css('overflow-y', 'hidden');
-    $(col).removeClass('shw-col-borders ' + colClass);
-    $(id).addClass('closed');
+    if (state === 'open') { openPanel(id, col, colClass); }
+    else { closePanel(id, col, colClass) }
 }
 function getMenuColumn(id) {
     return { 
@@ -59,6 +48,58 @@ function getColumnSpacerId(id) {
         '#int-opts': 'hide-int-bttm-border', 
     }[id];
 }
+/* ----- Open Panel(s) ----- */
+function openPanel(id, col, colClass) {
+    if (!bothPanelsOpen(id)) { cssOpenPanel(id, col, colClass);
+    } else { openVerticalPanels(id, col, colClass); }
+}
+function bothPanelsOpen(id) {
+    const otherPnl = id.includes('int') ? '#filter-opts' : '#int-opts';
+    return !$(otherPnl).hasClass('closed'); 
+}
+function cssOpenPanel(id, col, colClass) {
+    $(id).removeClass('closed');  
+    $(col).addClass('shw-col-borders ' + colClass);
+    window.setTimeout(function() { 
+        $(id).css('overflow-y', 'visible')}, 500);  
+}
+function openVerticalPanels(id, col, colClass) {
+    $('#fltr-int-panl-cntnr').attr('class', 'flex-row');
+    $('#filter-opts, #int-opts').removeClass('flex-row').addClass('flex-col');
+    updateFilterPanelStyles('vert');
+    updateIntListPanelStyles('vert');
+    cssOpenPanel(id, col, colClass);
+}
+function updateFilterPanelStyles(style) {
+    if (style == 'vert') { stackFilterPanel();
+    } else { spreadFilterPanel(); }
+}
+function stackFilterPanel() {
+    $('#filter-opts, #filter-col1').addClass('vert');
+}
+function spreadFilterPanel() {
+    // body...
+}
+function updateIntListPanelStyles(style) {
+    if (style == 'vert') { stackIntListPanel();
+    } else { spreadIntListPanel(); }
+}
+function stackIntListPanel() {
+    $('#int-opts, #int-lists, #list-details, #mod-list-pnl').addClass('vert');
+}
+function spreadIntListPanel() {
+    // body...
+}
+/* --- Close Panel(s) --- */
+function closePanel(id, col, colClass) {
+    cssClosePanel(id, col, colClass);
+}
+function cssClosePanel(id, col, colClass) {
+    $(id).css('overflow-y', 'hidden');
+    $(col).removeClass('shw-col-borders ' + colClass);
+    $(id).addClass('closed');
+}
+
 /* ---------------- SUBMIT AND SUCCESS METHODS -------------------------------*/
 export function submitUpdates(data, action, successFunc) {
     const envUrl = $('body').data("ajax-target-url");
