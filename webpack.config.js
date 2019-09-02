99ac35e213c181315e5910d3f5073d9c192143dc
srhLzbth <rnbwspctrm@gmail.com>
2019-09-02 11:33:59 -0700

    Fixed a couple bugs.

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,25 @@ const Encore = require('@symfony/webpack-encore');
 
 const autoProvidedVars = { L: 'leaflet', $: 'jquery' };
 
-/** ================= Create local development config ======================= */
+/** ================= CLI ======================= */
+//  --- DEV ---
+// yarn run encore dev --public-path="/batplant/web/build"
+//  --- PROD ---
+// yarn run encore production --public-path="/build"
+/** ================= Configuration ======================= */
 Encore
     // the project directory where all compiled assets will be stored
-    .setOutputPath('web/build/local')
+    .setOutputPath('web/build')
+
+
+   /* ==== DEV ==== */
+    .setPublicPath('/batplant/web/build')
+    
+   /* ==== PROD ==== */ 
     // the public path used by the web server to access the previous directory
-    .setPublicPath('/batplant/web/build/local')
+    // .setPublicPath('/build')
+
+
     /** The prefix isn't being recognized for some reason */
     .setManifestKeyPrefix('build')
     // allow legacy applications to use $/jQuery as an app variable 
@@ -34,6 +47,11 @@ Encore
     //         skipWaiting: true,
     //         importsDirectory: 'sw/'
     // }))
+    /** ------- Files to process ----------------- */
+    // .copyFiles({
+    //     from: './assets/uploads/publications'
+    //     to: 'publications/[path][name].[hash:8].[ext]'
+    // })
     /** ------- Site Js/Style Entries ----------------- */
     .addEntry('app', './assets/js/app/oi.js')
     .addEntry('db', './assets/js/db/db-page.js')
@@ -42,65 +60,13 @@ Encore
     // .createSharedEntry('libs', './assets/js/app/libs.js')
     // if the same module (e.g. jquery) is required by multiple entry files, they will require the same object.
     .enableSingleRuntimeChunk()
+    .splitEntryChunks()
 ; 
-const local = Encore.getWebpackConfig();
+const confg = Encore.getWebpackConfig();
 
-// Set a unique name for the config (needed to generate assets via cli!)
-local.name = 'local';
-// reset Encore to build the second config
-Encore.reset();
+// Change the kind of source map generated in development mode
+if (!Encore.isProduction()) {
+    confg.devtool = 'eval-source-map';
+}
 
-/** ======================= Create server config ============================ */
-Encore
-    // if the same module (e.g. jquery) is required by multiple entry files, they will require the same object.
-    .enableSingleRuntimeChunk()
-    // the project directory where all compiled assets will be stored
-    .setOutputPath('web/build/server')
-    // the public path used by the web server to access the previous directory
-    .setPublicPath('/build')
-    /** The prefix isn't being recognized for some reason */
-    .setManifestKeyPrefix('build')
-    // allow legacy applications to use $/jQuery as an app variable 
-    // Note: Doesn't work if js not processed through webpack
-    .autoProvidejQuery()
-    // enable source maps during development
-    .enableSourceMaps(!Encore.isProduction())
-    // empty the outputPath dir before each build
-    .cleanupOutputBeforeBuild()
-    // show OS notifications when builds finish/fail /** Stopped working and I don't know why. */
-    .enableBuildNotifications()
-    // filenames include a hash that changes whenever the file contents change
-    .enableVersioning()
-    // use this method to provide common global variables
-    .autoProvideVariables(autoProvidedVars)
-    // .addPlugin(
-    //     new WorkboxPlugin.GenerateSW({
-    //         // these options encourage the ServiceWorkers to get in there fast 
-    //         // and not allow any straggling "old" SWs to hang around
-    //         clientsClaim: true,
-    //         skipWaiting: true,
-    //         importsDirectory: 'sw/'
-    // }))
-    /** ------- Site Js/Style Entries ----------------- */
-    .addEntry('app', './assets/js/app/oi.js')
-    .addEntry('db', './assets/js/db/db-page.js')
-    .addEntry('feedback', './assets/js/misc/feedback-viewer.js')
-    .addEntry('pdfs', './assets/js/misc/view-pdfs.js')
-    // if the same module (e.g. jquery) is required by multiple entry files, they will require the same object.
-    .enableSingleRuntimeChunk()
-; 
-const server = Encore.getWebpackConfig();
-// Set a unique name for the config (needed to generate assets via cli!)
-server.name = 'server';
-// Remove the old version of uglify (doesn't parce es6)
-// server.plugins = server.plugins.filter(
-//     plugin => !(plugin instanceof webpack.optimize.UglifyJsPlugin)
-// );
-// Add the new one
-// server.plugins.push(new UglifyJsPlugin());
-
-// export the final configuration
-module.exports = [server, local];
-
-
-// Run [yarn run encore dev --config-name server] to generate assets for sites on server
+module.exports = confg;
