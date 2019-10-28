ae59941583a8cde85e6f6c0255f4f64d8a7d6878
srhLzbth <rnbwspctrm@gmail.com>
2019-10-28 14:38:18 -0700

    Moved save/exit button methods to new file

---
+++ assets/js/db/db-forms/form-ui/save-exit-bttns.js
@@ -0,0 +1,80 @@
+/**
+ * Returns a container with 'Create [Entity]' and 'Cancel' buttons bound to events
+ * specific to their form container @getBttnEvents, and a left spacer that 
+ * pushes the buttons to the bottom right of their form container.
+ *
+ * Exports:             Imported by:
+ *     buildFormBttns       form-ui
+ *     
+ */
+import * as _u from '../../util.js';
+import * as db_forms from '../db-forms.js';
+import * as _cmbx from '../combobox-util.js';
+
+let fP;
+
+export function buildFormBttns(entity, level, action, noShwFields, params) { 
+    fP = params;   
+    const cntnr = _u.buildElem("div", { class: "flex-row bttn-cntnr" });
+    const shwFields = noShwFields ? null : buildAddFieldsCheckbox(entity, level);
+    const spacer = $('<div></div>').css("flex-grow", 2);
+    const submitBttns = buildSubmitAndCancelBttns(level, action, entity);
+    $(cntnr).append([shwFields, spacer].concat(submitBttns));
+    return cntnr;
+}
+/** 
+ * Returns the html of a checkbox labeled 'Show all fields' that toggles the 
+ * form fields displayed between the default fields and all available.
+ * If there are no additional fields for the form, no checkbox is returned. 
+ * @return {elem} Checkbox and label that will 'Show all fields'
+ */
+function buildAddFieldsCheckbox(entity, level) {                                //console.log('fP = %O', fP);
+    if (fP.forms[level].confg.order.opt === false) { return; }
+    const cntnr = _u.buildElem('div', {class: 'all-fields-cntnr'});
+    const chckBox = _u.buildElem('input', { id: level+'-all-fields', 
+        type: 'checkbox', value: 'Show all fields' }) 
+    const lbl = _u.buildElem('label', { for: level+'-all-fields', 
+        text: 'Show all fields.' }); 
+    if (fP.forms.expanded[entity]) { chckBox.checked = true; }
+    $(chckBox).change(toggleShowAllFields.bind(chckBox, _u.lcfirst(entity), level));
+    _u.addEnterKeypressClick(chckBox);
+    $(cntnr).append([chckBox, lbl]);
+    return cntnr;
+}
+/** Returns the buttons with the events bound. */
+function buildSubmitAndCancelBttns(level, action, entity) {
+    const bttn = { create: "Create", edit: "Update" };
+    const events = getBttnEvents(entity, level);                                //console.log("events = %O", events);
+    const submit = buildFormButton(
+        'submit', level, bttn[action] + " " + _u.ucfirst(entity));
+    const cancel = buildFormButton('cancel', level, 'Cancel');
+    $(submit).attr("disabled", true).css("opacity", ".6").click(events.submit);
+    $(cancel).css("cursor", "pointer").click(events.cancel);
+    return [submit, cancel];
+}
+/** Returns a (submit or cancel) button for the form level. */
+function buildFormButton(action, level, val) {
+    return _u.buildElem("input", { id: level +'-'+action, 
+        class: "ag-fresh tbl-bttn", type: "button", value: val});
+}
+/**
+ * Returns an object with 'submit' and 'cancel' events bound to the passed level's
+ * form container.  
+ */
+function getBttnEvents(entity, level) {                                         //console.log("getBttnEvents for [%s] @ [%s]", entity, level);
+    return { 
+        submit: db_forms.getFormValuesAndSubmit.bind(null, '#'+level+'-form', level, entity), 
+        cancel: exitForm.bind(null, '#'+level+'-form', level, true, fP.forms[level].exitHandler) 
+    };
+}
+/**
+ * Removes the form container with the passed id, clears and enables the combobox,
+ * and contextually enables to parent form's submit button. Calls the exit 
+ * handler stored in the form's params object.
+ */
+function exitForm(formId, fLvl, focus, onExit, data) {                                  //console.log("               --exitForm id = %s, fLvl = %s, exitHandler = %O", formId, fLvl, fP.forms[fLvl].exitHandler);      
+    $(formId).remove();  
+    _cmbx.resetFormCombobox(fLvl, focus);
+    if (fLvl !== 'top') { db_forms.ifParentFormValidEnableSubmit(fLvl); }
+    onExit(data);
+}
