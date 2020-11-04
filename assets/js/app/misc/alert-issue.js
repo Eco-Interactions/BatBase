23a01e52f53046e7644c89644e3bb9cce6e48194
Sarah Elizabeth <rnbwspctrm@gmail.com>
2020-11-04 09:57:25 -0800

    Bug fixes.

--- assets/js/app/misc/alert-issue.js
+++ assets/js/app/misc/alert-issue.js
@@ -62,7 +62,7 @@ function setBasicStateContext() {
     if ($('body').data('this-url') !== '/search') { return; }
     const state = _table('tableState').get();
     const base = {focus: state.curFocus, view: state.curView};
-    Sentry.setContext('filter_state', Object.assign(base, _table('getCurrentFilterState')));
+    Sentry.setContext('filter_state', Object.assign(base, _table('getFilterState')));
 }
 function setErrorContext (errData) {
     Sentry.setContext('error_data', errData);
