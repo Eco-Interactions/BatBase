0280c9f55e1243674ba418c04967d34f19fae6a7
Sarah Elizabeth <rnbwspctrm@gmail.com>
2020-12-19 17:17:19 -0800

    Send Weekly Email Digest Class outline built.

---
+++ src/Command/SendWeeklyEmailDigestCommand.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class SendWeeklyEmailDigestCommand extends Command
+{
+    // the name of the command (the part after "bin/console")
+    protected static $defaultName = 'app:send-weekly-digest';
+
+/* ==================== CONSTRUCT/CONFIGURE COMMAND ================================================================= */
+    public function __construct(bool $requirePassword = false)
+    {
+        // best practices recommend to call the parent constructor first and
+        // then set your own properties. That wouldn't work in this case
+        // because configure() needs the properties set in this constructor
+        // $this->requirePassword = $requirePassword;
+
+        parent::__construct();
+    }
+
+    /**
+     * The configure() method is called automatically at the end of the command
+     * constructor. If your command defines its own constructor, set the properties
+     * first and then call to the parent constructor, to make those properties
+     * available in the configure() method
+     */
+    protected function configure()
+    {
+        // ...
+    }
+/* ==================== EXECUTE COMMAND ============================================================================= */
+    /** this method must return the "exit status code". 0: success, 1: error */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln(['Creating Weekly Digest', '======================','']);
+
+        $emailHtml = $this->buildDigestHtml();
+
+        try {
+            $this->sendDigestEmail($emailHtml);
+            return 0;
+        } catch (Exception $e) {
+            $output->writeln(['ERROR', $e->getMessage()]);
+            return 1;
+        }
+    }
+/* ==================== BUILD DIGEST HTML =================================== */
+    private function buildDigestHtml($value='')
+    {
+        # code...
+    }
+/* =================== SEND DIGEST EMAIL ==================================== */
+    private function sendDigestEmail($value='')
+    {
+        # code...
+    }
+}
+/* ==================== CONSOLE OUTPUT ============================================================================== */
+    // // outputs multiple lines to the console (adding "\n" at the end of each line)
+    // $output->writeln([
+    //     'User Creator',
+    //     '============',
+    //     '',
+    // ]);
+
+    // // the value returned by someMethod() can be an iterator (https://secure.php.net/iterator)
+    // // that generates and returns the messages with the 'yield' PHP keyword
+    // $output->writeln($this->someMethod());
+
+    // // outputs a message followed by a "\n"
+    // $output->writeln('Whoa!');
+
+    // // outputs a message without adding a "\n" at the end of the line
+    // $output->write('You are about to ');
+    // $output->write('create a user.');
