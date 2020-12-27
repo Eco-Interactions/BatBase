b18ccc1d6b5b412d5c7d5bbd12cba5e277315c9c
Sarah Elizabeth <rnbwspctrm@gmail.com>
2020-12-27 03:40:17 -0800

    [DB MIGRATION] SystemDate

---
+++ src/Migrations/Version20201227113702.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201227113702 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE system_date CHANGE description entity VARCHAR(255) NOT NULL, CHANGE date_val updated DATETIME NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE system_date CHANGE entity description VARCHAR(255) CHARACTER SET utf8 NOT NULL COLLATE `utf8_unicode_ci`, CHANGE updated date_val DATETIME NOT NULL');
+    }
+}
